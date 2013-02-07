7ce924bad2bec179e2cb074bf9a039b6262022e0
Andrew Crenshaw <avast.sarcasm@gmail.com>
2013-02-06 22:17:48 -0800

    Initial commit

---
+++ HW01/src/MathTrick.java
@@ -0,0 +1,17 @@
+/**
+ * Name: Andrew Crenshaw
+ * Approximate completion time: TODO: time
+ * References: None
+ */
+public class MathTrick {
+	public static void main(String[] args) {
+		//Get a positive integer
+		//Square it
+		//Add the origonal number
+		//Divide by the origonal number
+		//Add 17
+		//Subtract the orginal number
+		//Divide by 6
+		//Print the result
+	}
+}
